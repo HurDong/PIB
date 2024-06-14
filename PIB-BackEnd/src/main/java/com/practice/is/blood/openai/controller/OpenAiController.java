71f6d286fa75809918be664c4ac378f48ece86cd
HurDong <gjehdals456@gmail.com>
2024-06-15 02:44:47 +0900

    Feat : Get recruit information from jasoseol.com
    todo :
    - get daily and save daily recruit info
    - get other months instead of current month
    
    remember : we must maintain recent google chrome version

--- PIB-BackEnd/src/main/java/com/practice/is/blood/openai/controller/OpenAiController.java
+++ PIB-BackEnd/src/main/java/com/practice/is/blood/openai/controller/OpenAiController.java
@@ -1,13 +1,23 @@
 package com.practice.is.blood.openai.controller;
 
+import java.io.IOException;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,7 +31,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/v1/openai")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:8080")
+//@CrossOrigin(origins = "*")
 public class OpenAiController {
 	private static final Logger logger = LoggerFactory.getLogger(OpenAiController.class);
 
@@ -42,5 +52,44 @@ public class OpenAiController {
 		logger.info("유저 입력 : {}", userInput);
 		String response = openAiService.submitPrompt(userInput);
 		return ResponseEntity.ok(response);
+	}
+
+	@GetMapping("/crawl")
+	public List<String> crawl() {
+		List<String> companyNames = new ArrayList<>();
+		String url = "https://jasoseol.com/recruit"; // 크롤링할 웹 페이지 URL
+
+		try {
+			// ChromeDriver 경로 설정
+			String chromeDriverPath = new ClassPathResource("chromedriver.exe").getFile().getAbsolutePath();
+			System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+
+			WebDriver driver = new ChromeDriver();
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+			try {
+				// 웹 페이지 로드
+				driver.get(url);
+
+				// 페이지 소스를 가져와 Jsoup으로 파싱
+				String pageSource = driver.getPageSource();
+				Document doc = Jsoup.parse(pageSource);
+
+				// 회사명 추출
+				Elements companyElements = doc.select("a.company div.company-name span");
+				for (Element companyElement : companyElements) {
+					companyNames.add(companyElement.text());
+				}
+
+			} finally {
+				// WebDriver 종료
+				driver.quit();
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return companyNames;
 	}
 }
