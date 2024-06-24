7f4616dea9b5c48df7d6a9a6b5d6d23c07bd8b56
HurDong <gjehdals456@gmail.com>
2024-06-25 03:11:46 +0900

    Feat : init WebSocket Practice UI
    TODO :
    - show intuitive UI

--- PIB-FrontEnd/src/App.vue
+++ PIB-FrontEnd/src/App.vue
@@ -1,30 +1,82 @@
-<script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+    <div v-if="!connected">
+      <input v-model="username" placeholder="Enter your name" />
+      <button @click="connect">Join Chat</button>
+    </div>
+    <div v-else>
+      <div class="chat-window">
+        <div v-for="message in messages" :key="message.content">
+          <b>{{ message.sender }}:</b> {{ message.content }}
+        </div>
+      </div>
+      <input
+        v-model="message"
+        @keyup.enter="sendMessage"
+        placeholder="Type a message"
+      />
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<script>
+import SockJS from "sockjs-client";
+import Stomp from "stompjs";
+
+export default {
+  data() {
+    return {
+      username: "",
+      message: "",
+      messages: [],
+      stompClient: null,
+      connected: false,
+    };
+  },
+  methods: {
+    connect() {
+      const socket = new SockJS("http://localhost:8080/ws");
+      this.stompClient = Stomp.over(socket);
+      this.stompClient.connect({}, (frame) => {
+        this.connected = true;
+        console.log("Connected: " + frame);
+        this.stompClient.subscribe("/topic/public", (message) => {
+          const msg = JSON.parse(message.body);
+          this.messages.push(msg);
+        });
+
+        this.stompClient.send(
+          "/app/chat.addUser",
+          {},
+          JSON.stringify({ sender: this.username, type: "JOIN" })
+        );
+      });
+    },
+    sendMessage() {
+      if (this.message.trim() !== "") {
+        const chatMessage = {
+          sender: this.username,
+          content: this.message,
+          type: "CHAT",
+        };
+        this.stompClient.send(
+          "/app/chat.sendMessage",
+          {},
+          JSON.stringify(chatMessage)
+        );
+        this.message = "";
+      }
+    },
+  },
+};
+</script>
+
+<style>
+.chat-window {
+  height: 300px;
+  overflow-y: scroll;
+  border: 1px solid #ccc;
+  padding: 10px;
+  margin-bottom: 10px;
 }
 </style>
