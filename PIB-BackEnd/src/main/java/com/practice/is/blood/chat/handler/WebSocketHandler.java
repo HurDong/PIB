6c3d474d2e18e556ea49036ac21cd137b5804cdc
HurDong <gjehdals456@gmail.com>
2024-06-26 00:12:11 +0900

    Feat : User List, Temp LogIn&Logout, Show Timestamp
    todo
    - login and logout with database
    - can delete logout user from user list

--- PIB-BackEnd/src/main/java/com/practice/is/blood/chat/handler/WebSocketHandler.java
+++ PIB-BackEnd/src/main/java/com/practice/is/blood/chat/handler/WebSocketHandler.java
@@ -1,5 +1,6 @@
 package com.practice.is.blood.chat.handler;
 
+import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -8,29 +9,106 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class WebSocketHandler extends TextWebSocketHandler {
 
-	private final Set<WebSocketSession> sessions = new HashSet<>();
+    private final Set<WebSocketSession> sessions = new HashSet<>();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final Set<String> usernames = new HashSet<>();
 
-	@Override
-	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-		sessions.add(session);
-		System.out.println("Connected: " + session.getId());
-	}
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
+        System.out.println("Connected: " + session.getId());
+    }
 
-	@Override
-	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-		System.out.println("Received message: " + message.getPayload());
-		for (WebSocketSession s : sessions) {
-			if (s.isOpen()) {
-				s.sendMessage(new TextMessage("User " + session.getId() + ": " + message.getPayload()));
-			}
-		}
-	}
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        String payload = message.getPayload();
+        Message chatMessage = objectMapper.readValue(payload, Message.class);
 
-	@Override
-	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-		sessions.remove(session);
-		System.out.println("Disconnected: " + session.getId());
-	}
+        if ("join".equals(chatMessage.getType())) {
+            usernames.add(chatMessage.getUsername());
+            broadcastUserList();
+        } else if ("leave".equals(chatMessage.getType())) {
+            usernames.remove(chatMessage.getUsername());
+            broadcastUserList();
+        } else if ("message".equals(chatMessage.getType())) {
+            chatMessage.setTimestamp(Instant.now().toString());
+            for (WebSocketSession s : sessions) {
+                if (s.isOpen()) {
+                    s.sendMessage(new TextMessage(objectMapper.writeValueAsString(chatMessage)));
+                }
+            }
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessions.remove(session);
+        broadcastUserList();
+        System.out.println("Disconnected: " + session.getId());
+    }
+
+    private void broadcastUserList() throws Exception {
+        Message userMessage = new Message();
+        userMessage.setType("users");
+        userMessage.setUsers(new HashSet<>(usernames));
+
+        for (WebSocketSession s : sessions) {
+            if (s.isOpen()) {
+                s.sendMessage(new TextMessage(objectMapper.writeValueAsString(userMessage)));
+            }
+        }
+    }
+
+    private static class Message {
+        private String type;
+        private String username;
+        private String text;
+        private String timestamp;
+        private Set<String> users;
+
+        // Getters and setters
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getText() {
+            return text;
+        }
+
+        public void setText(String text) {
+            this.text = text;
+        }
+
+        public String getTimestamp() {
+            return timestamp;
+        }
+
+        public void setTimestamp(String timestamp) {
+            this.timestamp = timestamp;
+        }
+
+        public Set<String> getUsers() {
+            return users;
+        }
+
+        public void setUsers(Set<String> users) {
+            this.users = users;
+        }
+    }
 }
