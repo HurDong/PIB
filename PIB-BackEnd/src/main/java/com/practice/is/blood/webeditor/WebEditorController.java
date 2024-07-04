a81b7d4243798b73cdef4c0696b515275c25e065
허동민 <gjehdals456@gmail.com>
2024-07-04 12:58:39 +0900

    :sparkles: add webeditor controller

---
+++ PIB-BackEnd/src/main/java/com/practice/is/blood/webeditor/WebEditorController.java
@@ -0,0 +1,13 @@
+package com.practice.is.blood.webeditor;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/webeditor")
+public class WebEditorController {
+
+}
