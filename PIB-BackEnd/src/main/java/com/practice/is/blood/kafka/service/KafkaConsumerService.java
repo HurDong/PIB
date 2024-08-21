1a76061384310983a730844bfb255b81e1ac09cc
허동민 <gjehdals456@gmail.com>
2024-08-21 16:26:13 +0900

    :zap: add senderId to configure who sends message

--- PIB-BackEnd/src/main/java/com/practice/is/blood/kafka/service/KafkaConsumerService.java
+++ PIB-BackEnd/src/main/java/com/practice/is/blood/kafka/service/KafkaConsumerService.java
@@ -3,6 +3,8 @@ package com.practice.is.blood.kafka.service;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.PartitionOffset;
 import org.springframework.kafka.annotation.TopicPartition;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -10,18 +12,19 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class KafkaConsumerService {
-	@KafkaListener(topicPartitions = @TopicPartition(topic = "user-events", partitionOffsets = @PartitionOffset(partition = "0", initialOffset = "0")), groupId = "user-consumer-group")
-	public void listenToPartition0(String message) {
-		System.out.println("0 파티션 메시지 수신 :  " + message);
-	}
 
-	@KafkaListener(topicPartitions = @TopicPartition(topic = "user-events", partitionOffsets = @PartitionOffset(partition = "1", initialOffset = "0")), groupId = "user-consumer-group")
-	public void listenToPartition1(String message) {
-		System.out.println("1 파티션 메시지 수신 : " + message);
-	}
+    @KafkaListener(topicPartitions = @TopicPartition(topic = "user-events", partitionOffsets = @PartitionOffset(partition = "0", initialOffset = "0")), groupId = "user-consumer-group")
+    public void listenToPartition0(@Payload String message, @Header("kafka_receivedMessageKey") String senderId) {
+        System.out.println("송신자 ID: " + senderId + " | 0 파티션 메시지 수신: " + message);
+    }
 
-	@KafkaListener(topicPartitions = @TopicPartition(topic = "user-events", partitionOffsets = @PartitionOffset(partition = "2", initialOffset = "0")), groupId = "user-consumer-group")
-	public void listenToPartition2(String message) {
-		System.out.println("2 파티션 메시지 수신 : " + message);
-	}
+    @KafkaListener(topicPartitions = @TopicPartition(topic = "user-events", partitionOffsets = @PartitionOffset(partition = "1", initialOffset = "0")), groupId = "user-consumer-group")
+    public void listenToPartition1(@Payload String message, @Header("kafka_receivedMessageKey") String senderId) {
+        System.out.println("송신자 ID: " + senderId + " | 1 파티션 메시지 수신: " + message);
+    }
+
+    @KafkaListener(topicPartitions = @TopicPartition(topic = "user-events", partitionOffsets = @PartitionOffset(partition = "2", initialOffset = "0")), groupId = "user-consumer-group")
+    public void listenToPartition2(@Payload String message, @Header("kafka_receivedMessageKey") String senderId) {
+        System.out.println("송신자 ID: " + senderId + " | 2 파티션 메시지 수신: " + message);
+    }
 }
